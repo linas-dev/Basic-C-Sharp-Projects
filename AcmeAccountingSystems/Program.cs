1e0dff749014b65c9d1220ed6d253bfbe459c88a
linassmita <linas.smita@gmail.com>
2022-07-27 21:11:00 +0100

    Writing a console application

---
+++ AcmeAccountingSystems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccountingSystems
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember,  we're \"accounting\" on you");
+        }
+    }
+}
